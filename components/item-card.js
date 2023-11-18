0029eb9ee456b6a1bfe80eaeae6d4b472cbdaf24
Abhishekgd9678 <abhishekgd2003@gmail.com>
2023-11-18 22:34:07 +0530

    none

--- components/item-card.js
+++ components/item-card.js
@@ -3,32 +3,30 @@ import Images from '../images.js';
 import data from "./data.json"
 
 const Card = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const prev = () => {
-    const newIndex = currentIndex - 3 >= 0 ? currentIndex - 3 : Images.length - 3;
-    setCurrentIndex(newIndex);
-  };
-
-  const next = () => {
-    const newIndex = currentIndex + 3 < Images.length ? currentIndex + 3 : 0;
-    setCurrentIndex(newIndex);
-  };
-
-  const displayedImages = Images.concat(Images.slice(0, 3)).slice(currentIndex, currentIndex + 3);
-console.log(data);
+  
   return (
 
     <div className="Card">
-      <button className="btn" onClick={prev}>
+      <button className="btn">
         {"<"}
       </button>
-      {displayedImages.map((image, index) => (
-        <div key={index} className="image-div">
-          <img className="round-image" src={image} alt={`Image ${currentIndex + index}`} />
-        </div>
-      ))}
-      <button className="btn" onClick={next}>
+
+      {data.data.map((e)=>{
+        return (
+          <>
+          <div className='image-div'>
+       
+            <img className="round-image" src={e.link}/>
+             <h4 className='image-des'>{e.desp}</h4>
+        
+         
+          </div>
+          
+          </>
+        )
+      })}
+     
+      <button className="btn" >
        {">"}
       </button>
     </div>
