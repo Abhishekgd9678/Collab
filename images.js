818eb428ba3af76e526e5d25a3552e976b62d41d
Abhishekgd9678 <abhishekgd2003@gmail.com>
2023-11-18 15:54:51 +0530

    carousel2.0

--- images.js
+++ images.js
@@ -3,12 +3,10 @@ const Images=[
    "https://cdn-icons-png.flaticon.com/512/1574/1574824.png",
    "https://cdn-icons-png.flaticon.com/512/9954/9954768.png",
 
-"https://media.istockphoto.com/id/1297349747/photo/hot-air-balloons-flying-over-the-botan-canyon-in-turkey.jpg?s=612x612&w=0&k=20&c=kt8-RRzCDunpxgKFMBBjZ6jSteetNhhSxHZFvHQ0hNU=",
-"https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630",
-   "https://images.unsplash.com/photo-1541963463532-d68292c34b19?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8fA%3D%3D",
-   "https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630",
-"https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8=",
-"https://media.istockphoto.com/id/1297349747/photo/hot-air-balloons-flying-over-the-botan-canyon-in-turkey.jpg?s=612x612&w=0&k=20&c=kt8-RRzCDunpxgKFMBBjZ6jSteetNhhSxHZFvHQ0hNU="
+"https://cdn-icons-png.flaticon.com/512/1682/1682194.png",
+"https://cdn-icons-png.flaticon.com/512/2713/2713563.png",
+"https://cdn.pixabay.com/photo/2014/12/22/00/05/green-576756_960_720.png",
+"https://cdn-icons-png.flaticon.com/512/7192/7192728.png"
 
 ]
 export default Images;
