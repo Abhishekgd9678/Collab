4af25c1be2118cb03c390889dd162d061f0e2966
Ajay-S-Biradar <sbmunnu@gmail.com>
2023-11-18 22:03:37 +0530

    carousel added

--- images.js
+++ images.js
@@ -5,8 +5,8 @@ const Images=[
 
 "https://cdn-icons-png.flaticon.com/512/1682/1682194.png",
 "https://cdn-icons-png.flaticon.com/512/2713/2713563.png",
+"https://cdn-icons-png.flaticon.com/512/7192/7192728.png",
 "https://cdn.pixabay.com/photo/2014/12/22/00/05/green-576756_960_720.png",
-"https://cdn-icons-png.flaticon.com/512/7192/7192728.png"
 
 ]
 export default Images;
